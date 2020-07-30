5f07f5cd0171e5dd57ee0db0c24d474c22498817
Varshika-dadhich <varshika.18jics043@jietjodhpur.ac.in>
2020-07-30 14:37:37 +0530

    2nd commit

--- script.js
+++ script.js
@@ -30,16 +30,12 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-(function(windows){ 
- greeter.names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-greeter.greet=  function(){
-for (var i in names) {
+
 
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
@@ -49,18 +45,40 @@ for (var i in names) {
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
   // var firstLetter =
-  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   
-  if (firstLetter==j) {
-    window.byeSpeaker.speak(name[i]);
+  
+
+(function () {
+
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  
+  for (var i in names) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+  
+    if (firstLetter === 'j') {
+      byeSpeaker(names[i]);
+    }
+    else {
+      helloSpeaker(names[i]);
+    }
+  }
+  
+  })();
+
+  /*(function(){ 
+ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (var i in names) {
+ var firstLetter = names[i].charAt(0).toLowerCase();
+ if (firstLetter==='j') {
+    byeSpeaker(name[i]);
   } else {
-    window.helloSpeaker.speak(name[i]);
+    helloSpeaker(name[i]);
   }
 }
-window.greeter=greeter;
-}})(windows);
+})();
+*/
